8139154c147d614919cdfe106bb6ef0c39de33b7
Sumesh <dksumesh981@email.com>
2024-10-14 21:02:04 +0530

    Cleaned code

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Reading from "./pages/Reading"
 import Finished from "./pages/Finished"
 import WillRead from "./pages/WillRead"
 
-// Will Read, Currently Reading, Finished --> Will See, Watch List, Watched
-
 function App() {
   return (
     <>
